172b565302ee9af62e385628379772e2b58a3a17
justinneff5 <59897146+justinneff5@users.noreply.github.com>
2023-01-21 15:47:41 -0600

    note key w/ dictionary

--- client/src/app/instruments/midi-instrument.ts
+++ client/src/app/instruments/midi-instrument.ts
@@ -5,15 +5,30 @@ export class MidiInstrument {
     public soundpack?: AudioBuffer; //i think this is the right type
     public sound: string;
     public engine: any;
+    private keyDict: Record<string, string>;
     constructor(name: string) {
         this.name = name;
         this.sound = "something.mp3" //to load for later
         this.engine = new Tone.Synth().toDestination();
+        this.keyDict = {
+            "a": "C4",
+            "w": "C#4",
+            "s": "D4",
+            "e": "D#4",
+            "d": "E4",
+            "f": "F4",
+            "t": "F#4",
+            "g": "G4",
+            "y": "G#4",
+            "h": "A4",
+            "u": "A#4",
+            "j": "B4",
+            "k": "C5"
+        }
     }
     Play(noteKey : string, length: number) {
        //code to emit sound 
         let now = Tone.now();
-        let note = noteKey.toUpperCase() + '4';
-        this.engine.triggerAttackRelease(note, length, now);
+        this.engine.triggerAttackRelease(this.keyDict[noteKey], length, now);
     }
 }
