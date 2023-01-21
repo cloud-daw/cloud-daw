6a76c1d790ed7baec7eb4b6a1cd4c9621f0cd348
justinneff5 <59897146+justinneff5@users.noreply.github.com>
2023-01-21 15:40:17 -0600

    instrument format w/ host listener

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,7 +1,8 @@
 import { Component } from '@angular/core';
 import { ApiHttpService } from './services/httpservice.service';
 import { Observable, of } from 'rxjs';
-import * as Tone from 'tone';
+import { MidiInstrument } from './instruments/midi-instrument'; //for now, do here -> in future, put in track
+import { HostListener } from '@angular/core'; //for now, put in track later (to be trapped w/ focus from here)
 
 @Component({
   selector: 'app-root',
@@ -10,9 +11,16 @@ import * as Tone from 'tone';
 })
 export class AppComponent {
   title = 'CloudDaw';
+  @HostListener('document:keypress', ['$event'])
+  handleKeyboardEvent(event: KeyboardEvent) {
+    console.log(event);
+    this.synth.Play(event.key, 1);
+  }
+
   constructor(public ApiHttpService: ApiHttpService) { 
     this.status = 'no stat update';
     this.status$ = this.ApiHttpService.getStatus() //SAMPLE: grabs observable return from server
+    this.synth = new MidiInstrument("test");
   }
   status: string;
   status$: Observable<any>;
@@ -21,7 +29,9 @@ export class AppComponent {
   tempo: number = 120; //default
   signature: number[] = [1, 2, 3, 4]; //default 4/4
   timeoutValue: number = (60 / this.tempo) * 1000; //in ms
-  synth: any = new Tone.Synth().toDestination();
+  synth: MidiInstrument;
+
+  
 
   show() { // test, example method for backend comms
     this.status$.subscribe({
@@ -66,13 +76,6 @@ export class AppComponent {
     console.log('volume changed');
     console.log(event);
     this.masterVolume = event;
-  }
-
-  playTest() {
-    let now = Tone.now();
-    this.synth.triggerAttackRelease("C4", "8n", now)
-    this.synth.triggerAttackRelease("E4", "8n", now + 0.5)
-    this.synth.triggerAttackRelease("G4", "8n", now + 1)
   }
 
 }
--- client/src/app/instruments/midi-instrument.ts
+++ client/src/app/instruments/midi-instrument.ts
@@ -1,10 +1,19 @@
-class MidiInstrument {
+import * as Tone from 'tone';
+
+export class MidiInstrument {
     public name: string;
     public soundpack?: AudioBuffer; //i think this is the right type
+    public sound: string;
+    public engine: any;
     constructor(name: string) {
         this.name = name;
+        this.sound = "something.mp3" //to load for later
+        this.engine = new Tone.Synth().toDestination();
     }
-    Play(note : number) {
+    Play(noteKey : string, length: number) {
        //code to emit sound 
+        let now = Tone.now();
+        let note = noteKey.toUpperCase() + '4';
+        this.engine.triggerAttackRelease(note, length, now);
     }
 }
