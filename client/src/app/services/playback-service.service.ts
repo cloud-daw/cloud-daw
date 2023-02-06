b571fdce6d4c5cf5a067e1bc4fded96a7b2d3509
justinneff5 <59897146+justinneff5@users.noreply.github.com>
2023-02-05 20:57:47 -0600

    documentation, cleanup

--- client/src/app/services/playback-service.service.ts
+++ client/src/app/services/playback-service.service.ts
@@ -2,15 +2,25 @@ import { Note } from '../models/recording/note';
 import { MidiInstrument } from '../models/instruments/midi-instrument'
 import * as Tone from 'tone';
 
+/**
+ * Initializes playback data for a given synth's recording
+ * @param data Note array from recording
+ * @param synth Instrument to play
+ */
 export function SchedulePlayback(data: Note[], synth: MidiInstrument) {
     data.forEach((note) => {
         Tone.Transport.schedule((time) => {
-            console.log(MakeDuration(note.attack, note.release));
             synth.NotePlayback(note.value, MakeDuration(note.attack, note.release));
         }, note.attack);
     })
 }
 
+/**
+ * Calculates time held of note
+ * @param attack attack of note from recording array
+ * @param release release of note from recording array
+ * @returns a bars:beats:sixteenths length of the note
+ */
 function MakeDuration(attack: string, release: string) {
     let attackMBS = attack.split(':');
     let releaseMBS = release.split(':');
