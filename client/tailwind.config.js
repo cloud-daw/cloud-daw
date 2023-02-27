39ea242b318d91d87fd20143b4a62d7245c6a2be
michaelyang12 <54558964+michaelyang12@users.noreply.github.com>
2023-02-27 15:43:28 -0600

    working on tailwind colors

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -2,6 +2,19 @@
 module.exports = {
   content: ["./src/**/*.{html,ts,js}"],
   theme: {
+    colors: {
+      textColor: '##D5D5D5',
+      midiContainerBg: '',
+      controlBarBg: '',
+      trackContainerBg: '',
+      trackColorDefault: '',
+      trackColorSelected: '',
+      trackColorHover: '',
+      limeGreen: '#00ff62',
+      gridLineColor: '#303030',
+      pianoBg: '#303030',
+      'white': '#ffffff',
+    },
     extend: {},
   },
   plugins: [],
