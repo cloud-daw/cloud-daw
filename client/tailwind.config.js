2cb4a15ec98204417eb487317b8653a3b2a41ac7
michaelyang12 <54558964+michaelyang12@users.noreply.github.com>
2023-02-27 15:57:17 -0600

    style changes

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -2,20 +2,21 @@
 module.exports = {
   content: ["./src/**/*.{html,ts,js}"],
   theme: {
-    colors: {
-      textColor: '##D5D5D5',
-      midiContainerBg: '',
-      controlBarBg: '',
-      trackContainerBg: '',
-      trackColorDefault: '',
-      trackColorSelected: '',
-      trackColorHover: '',
-      limeGreen: '#00ff62',
-      gridLineColor: '#303030',
-      pianoBg: '#303030',
-      'white': '#ffffff',
+    extend: {
+      colors: {
+        textColor: '##D5D5D5',
+        midiContainerBg: '',
+        controlBarBg: '',
+        trackContainerBg: '',
+        trackColorDefault: '',
+        trackColorSelected: '',
+        trackColorHover: '',
+        limeGreen: '#00ff62',
+        'gridLineColor': '#303030',
+        'measureLineColor': '#646464',
+        pianoBg: '#303030',
+      },
     },
-    extend: {},
   },
   plugins: [],
 };
