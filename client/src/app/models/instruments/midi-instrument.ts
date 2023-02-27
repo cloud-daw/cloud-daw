439016eb475dd3ca7209dc8dea096744e63fb66f
michaelyang12 <54558964+michaelyang12@users.noreply.github.com>
2023-02-27 14:59:04 -0600

    octave lower to sound better

--- client/src/app/models/instruments/midi-instrument.ts
+++ client/src/app/models/instruments/midi-instrument.ts
@@ -17,8 +17,8 @@ export class MidiInstrument {
     constructor(name: string) {
         this.name = name;
         this.sound = "AM PolySynth" //to load for later
-        this.instrument = new Tone.PolySynth(Tone.AMSynth).toDestination();
-        this.keyDict = MakeKeyDict(4);
+        this.instrument = new Tone.PolySynth().toDestination();
+        this.keyDict = MakeKeyDict(3);
         this.isPlaying = false;
         this.attack = 0;
         this.release = 0.1;
