4a0c4ddc17e037984437dcc2aaf9338e1870b2b4
michaelyang12 <54558964+michaelyang12@users.noreply.github.com>
2023-03-19 21:43:46 -0500

    sg

--- client/src/app/components/notes/midi-note/midi-note.component.ts
+++ client/src/app/components/notes/midi-note/midi-note.component.ts
@@ -9,12 +9,25 @@ import { Note } from 'src/app/models/recording/note';
 export class MidiNoteComponent {
   @Input() data: Note = new Note('', 0);
   @Input() vw: number = 0;
+  @Input() bars: number = 16;
 
   public width: number = 0;
   public widthCSS: string = `${this.width}vw`;
 
+  convertBBSToPosition(position: string) {
+    let strPosArr: string[] = position.split(':')
+    const bar = parseInt(strPosArr[0])
+    const beat = parseInt(strPosArr[1])
+    const sixteenth = parseFloat(strPosArr[2])
+    const bbsInterval = this.vw / (this.bars * 16)
+    const normedBar = 16 * (bar - 1);
+    const normedBeat = 4 * beat;
+    const bbsSum = normedBar + normedBeat + sixteenth
+    return bbsSum * bbsInterval; //as VW
+  }
+
   getWidth() {
-    const start: number = parseInt(this.data.attack + '');
+    const start: number = this.convertBBSToPosition(this.data.attack + '');
     const end: number = parseInt(this.data.release + '');
     
     this.width;
